fdc2fba3b0563ce26fa0dd79e2d6d57e37239d02
R4chel <rae84@cornell.edu>
2021-03-16 20:54:11 -0400

    tick

--- src/app.rs
+++ src/app.rs
@@ -4,12 +4,15 @@ use crate::circle::{Circle, ViewWindow};
 
 pub struct App {
     link: ComponentLink<Self>,
+    max_position_delta: f64,
     view_window: ViewWindow,
     circles: Vec<Circle>,
+    history: Vec<Circle>,
 }
 
 pub enum Msg {
     AddCircle,
+    Tick,
 }
 
 impl App {
@@ -24,11 +27,23 @@ impl App {
             <div>
                 <p>{ "Hello world!" }</p>
                 <button onclick=self.link.callback(|_| Msg::AddCircle)>{"+"}</button>
+                <button onclick=self.link.callback(|_| Msg::Tick)>{"🦶"}</button>
 
                 <svg width={self.view_window.x_max} height={self.view_window.y_max} viewBox={format!("{} {} {} {}", self.view_window.x_min, self.view_window.y_min, self.view_window.x_max, self.view_window.y_max)} fill="none" xmlns="http://www.w3.org/2000/svg">
+
+            { self.history.iter().map(App::view_circle).collect::<Html>() }
             { self.circles.iter().map(App::view_circle).collect::<Html>() }
+
                 </svg>
                 </div>
+        }
+    }
+
+    pub fn tick(&mut self) -> () {
+        for circle in self.circles.iter_mut() {
+            let clone = circle.clone();
+            self.history.push(clone);
+            circle.update(&self.view_window, self.max_position_delta);
         }
     }
 
@@ -45,14 +60,16 @@ impl Component for App {
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
         let view_window = ViewWindow {
             x_min: 0.0,
-            x_max: 100.0,
+            x_max: 1000.0,
             y_min: 0.0,
-            y_max: 100.0,
+            y_max: 1000.0,
         };
         let mut app = App {
             link,
             view_window,
+            max_position_delta: 2.0,
             circles: vec![],
+            history: vec![],
         };
         app.add_circle();
         app.add_circle();
@@ -63,6 +80,10 @@ impl Component for App {
         match msg {
             Msg::AddCircle => {
                 self.add_circle();
+                true
+            }
+            Msg::Tick => {
+                self.tick();
                 true
             }
         }
