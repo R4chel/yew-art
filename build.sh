a0b810c6114995ebf78488302a9567a57ed0c7f7
R4chel <rae84@cornell.edu>
2021-03-21 00:07:39 -0400

    add trash button

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-cd $(dirname $0)
-set -ex
-outdir=web-gen
-project=yew_art
-mkdir -p $outdir
-cargo +nightly build --target wasm32-unknown-unknown
-wasm-bindgen --target web target/wasm32-unknown-unknown/debug/$project.wasm --out-dir $outdir
-cp static/* $outdir/
